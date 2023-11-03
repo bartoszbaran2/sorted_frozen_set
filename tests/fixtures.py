36710b3b88fc0b6b4df49bc78d15d007e4e206b2
Bartosz Baran <bartosz.baran192@gmail.com>
2023-11-03 11:06:01 +0100

    update docstring for fixtures

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -4,15 +4,17 @@ from sorted_frozen_set.sorted_frozen_set import SortedFrozenSet
 
 @pytest.fixture(scope='function')
 def s():
-    """return SortedFrozenSEt object"""
+    """return SortedFrozenSet object"""
     return SortedFrozenSet([6, 7, 3, 9])
 
 
 @pytest.fixture
 def s1():
+    """return SortedFrozenSet object"""
     return SortedFrozenSet([7, 2, 1, 1, 9])
 
 
 @pytest.fixture
 def s2():
+    """return SortedFrozenSet object"""
     return SortedFrozenSet([1, 4, 9, 13, 15])
